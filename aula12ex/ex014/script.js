3992774dd589003e964087defdb5085baeedaed3
Renato Pereira de Oliveira <renatopereiradeoliveira83@gmail.com>
2022-02-06 15:47:40 -0300

    atualizando javascript
    atualizado

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,7 +3,7 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-//var hora = 22
+//var hora = 14
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >=0 && hora < 12) {
         // BOM DIA!
